23baf2e0004d4cc2b7ad7ba95ea75fe8cc561da4
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-03-09 13:47:18 -0800

    stubbing submit form

--- react-mood-tracker/src/components/mood-note/MoodNote.jsx
+++ react-mood-tracker/src/components/mood-note/MoodNote.jsx
@@ -4,7 +4,9 @@ export default class MoodNote extends Component{
     render(){
         return(
             <div>
-                <p>My mood was {this.props.points</p>
+                <p>My mood was {this.props.points} on {this.props.date}</p>
+
+                <p>My thoughts: {this.props.note}</p>
             </div>
         )
     }
--- react-mood-tracker/src/components/mood-tracker/MoodTracker.jsx
+++ react-mood-tracker/src/components/mood-tracker/MoodTracker.jsx
@@ -1,5 +1,7 @@
 import { Component } from "react";
 import MoodPoints from '../mood-points/MoodPoints.jsx';
+import MoodNote from '../mood-note/MoodNote.jsx';
+import placeHolderNotes from "./placeHolderNotes.js"
 export default class MoodTracker extends Component {
     // create state and set initial state
     // constructor(props) {
@@ -23,6 +25,7 @@ export default class MoodTracker extends Component {
             }
         })
     }
+    
     handleDecreaseMood = () => {
         this.setState((prevState) => {
             return {
@@ -30,13 +33,47 @@ export default class MoodTracker extends Component {
             }
         })
     }
+    
+    handleInputChange = (e) => {
+        console.log(e.target.value)
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('e')
+    }
     render () {
+        const noteComponents = placeHolderNotes.map((placeHolderNote, index) => {
+            return (
+                <MoodNote
+                    key={`note ${index}`}
+                    points={placeHolderNote.points}
+                    date={placeHolderNote.date}
+                    note={placeHolderNote.note}
+                />
+            )
+        })        
         return (
             <div>
                 <MoodPoints points={this.state.points} />
 
                 <button onClick={this.handleIncreaseMood}>+</button>
                 <button onClick={this.handleDecreaseMood}>-</button>
+                <h3>My Notes:</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor='note-input'>New Note:</label>
+                    <input
+                        id='note-input'
+                        type='text'
+                        placeholder='how ya feelin?'
+                        onChange={this.handleInputChange}
+                    />
+                    <input
+                    type='submit'
+                    value='Save Note'
+                    />
+                </form>
+                {noteComponents}
             </div>
         )
     }
